fd86c24d9e66edc149125d3986e0992cbde5a295
Paul Ralyea <047par@gmail.com>
2020-03-20 18:57:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,8 @@ planeteer_calls = planeteer_calls.map {|name| "#{name.capitalize}!"
 }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
+  
 end
 
 def find_the_cheese# code an argument here
